b11bd1abe082e2537adb58cf8154628cec58e921
yifanzhang-pro <zhangyif21@mails.tsinghua.edu.cn>
2024-11-06 22:50:29 +0100

    minor

--- openrlhf/trainer/rm_trainer.py
+++ openrlhf/trainer/rm_trainer.py
@@ -82,7 +82,7 @@ class RewardModelTrainer(ABC):
                 entity=strategy.args.wandb_org,
                 project=strategy.args.wandb_project,
                 group=strategy.args.wandb_group,
-                name="RM_M_" + str(strategy.args.pretrain) + "_D_" + str(strategy.args.dataset) + "_" + str(strategy.args.wandb_run_name),
+                name="RM_M_" + str(strategy.args.pretrain) + "_D_" + str(strategy.args.dataset) + "_" + str(strategy.args.max_epochs) + "epoch" + "_" + str(strategy.args.wandb_run_name),
                 config=strategy.args.__dict__,
                 reinit=True,
             )
