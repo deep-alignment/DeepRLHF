ee5599f64fb5af4ffe7f62c508b9b908558d5e54
yifanzhang-pro <zhangyif21@mails.tsinghua.edu.cn>
2024-11-24 18:24:18 +0100

    minor

--- rewardbench_eval/run_rm_rewardbench.sh
+++ rewardbench_eval/run_rm_rewardbench.sh
@@ -1,13 +1,13 @@
 export CUDA_VISIBLE_DEVICES=0
 python run_rm_rewardbench.py \
---model '../checkpoint/Skywork-Reward-Llama-3.1-8B-v0.2-GPM' \
+--model '../checkpoint/Llama-3.1-8B-Instruct-GPM-dim2' \
 --tokenizer 'meta-llama/Llama-3.1-8B-Instruct' \
 --chat_template raw \
 --is_custom_model \
 --do_not_save \
 --model_name "model" \
 --batch_size 32 \
---value_head_dim 64 \
+--value_head_dim 2 \
 --max_length 4096 \
 --is_general_preference \
 --is_preference_embedding_normalized \
@@ -18,3 +18,4 @@ python run_rm_rewardbench.py \
 # Optional flags:
 # --bf16 \
 # --flash_attn \
+# --model '../checkpoint/Skywork-Reward-Llama-3.1-8B-v0.2-GPM-dim2' \
