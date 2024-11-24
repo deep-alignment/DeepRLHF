e1e7f1509ceed214f7c54cd404b39f39a7b30f06
yifanzhang-pro <zhangyif21@mails.tsinghua.edu.cn>
2024-11-24 19:01:20 +0100

    minor

--- rewardbench_eval/run_rm_rewardbench.sh
+++ rewardbench_eval/run_rm_rewardbench.sh
@@ -1,12 +1,13 @@
+MODEL_NAME="Llama-3.1-8B-Instruct-GPM-dim2"
 export CUDA_VISIBLE_DEVICES=0
 python run_rm_rewardbench.py \
---model '../checkpoint/Llama-3.1-8B-Instruct-GPM-dim2' \
+--model "../checkpoint/${MODEL_NAME}" \
 --tokenizer 'meta-llama/Llama-3.1-8B-Instruct' \
 --chat_template raw \
 --is_custom_model \
 --do_not_save \
---model_name "model" \
---batch_size 32 \
+--model_name "${MODEL_NAME}" \
+--batch_size 16 \
 --value_head_dim 2 \
 --max_length 4096 \
 --is_general_preference \
